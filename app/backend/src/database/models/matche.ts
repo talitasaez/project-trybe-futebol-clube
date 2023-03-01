ffa071b4e9b89377474e864015885eec935ba4c2
talitasaez <tltsaez@gmail.com>
2023-03-01 19:57:26 -0300

    req 13 não sei se passou meus testes no local nao rodam

---
+++ app/backend/src/database/models/matche.ts
@@ -0,0 +1,48 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Matches extends Model {
+  declare id: number;
+
+  declare homeTeamId: number;
+
+  declare homeTeamGoals: number;
+
+  declare awayTeamId: number;
+
+  declare awayTeamGoals: number;
+
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  homeTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+
+}, { sequelize: db, timestamps: false, modelName: 'matches', underscored: true });
+
+export default Matches;
