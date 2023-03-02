92156d461fda3c070061cc819f165d1e71d479d0
talitasaez <tltsaez@gmail.com>
2023-03-01 21:13:56 -0300

    requisito 15 incompleto

--- app/backend/src/Controller/MatchController.ts
+++ app/backend/src/Controller/MatchController.ts
@@ -1,11 +1,11 @@
-// import { Request, Response } from 'express';
-// import ServiceMatch from '../service/MatchService';
+import { Request, Response } from 'express';
+import ServiceMatch from '../service/MatchService';
 
-// export default class ControllerMatch {
-//   static async getAllMatches(req: Request, res: Response) {
-//     const { inProgress } = req.query;
-//     const matches = await ServiceMatch.getAllMatches(inProgress as string);
+export default class ControllerMatch {
+  static async getAllMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    const matches = await ServiceMatch.getAllMatches(inProgress as string);
 
-//     return res.status(200).json(matches);
-//   }
-// }
+    return res.status(200).json(matches);
+  }
+}
